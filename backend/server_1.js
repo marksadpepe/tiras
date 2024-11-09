74be5926ffce0c906a128aa896657cc6300589b5
Mark Parker <marksadpepe@gmail.com>
2024-11-09 21:09:21 +0200

    renamed to server 1

--- backend/server_1.js
+++ backend/server_1.js
@@ -3,12 +3,20 @@ require("dotenv").config();
 const urlPkg = require("url");
 const http = require("http");
 const sock = require("socket.io");
+const dgram = require("dgram");
 
-const {FRONTEND_URL} = process.env;
-const PORT = parseInt(process.env.SERVER_PORT ?? "");
+// TODO: move parsing logic to another module
+const {FRONTEND_URL, SERVER_UDP_HOST} = process.env;
+const PORT = parseInt(process.env.SERVER_HTTP_PORT ?? "");
+const UDP_PORT = parseInt(process.env.SERVER_UDP_PORT ?? "");
 
 if (!FRONTEND_URL) {
   console.error("Frontend URL was not specified");
+  process.exit(1);
+}
+
+if (!SERVER_UDP_HOST) {
+  console.error("UDP host was not specified");
   process.exit(1);
 }
 
@@ -17,6 +25,12 @@ if (typeof PORT !== "number" || isNaN(PORT)) {
   process.exit(1);
 }
 
+if (typeof UDP_PORT !== "number" || isNaN(UDP_PORT)) {
+  console.error("Incorrect UDP port format");
+  process.exit(1);
+}
+
+// NOTE: should I move this to another module?
 const requestHandler = (req, res) => {
   const parsedUrl = urlPkg.parse(req.url, true);
   const path = parsedUrl.pathname;
@@ -39,6 +53,7 @@ const requestHandler = (req, res) => {
   res.end(JSON.stringify(data));
 };
 
+const udpSocket = dgram.createSocket("udp4");
 const httpServer = http.createServer(requestHandler);
 const io = new sock.Server(httpServer, {
   cors: {
@@ -50,7 +65,8 @@ io.on("connection", (socket) => {
   console.log(`${socket.id} - connected`);
 
   socket.on("form-message", (message) => {
-    const {title, body} = message;
+    const messageBuffer = Buffer.from(JSON.stringify(message));
+    udpSocket.send(messageBuffer, UDP_PORT, SERVER_UDP_HOST);
   });
 
   socket.on("disconnect", () => {
@@ -59,5 +75,5 @@ io.on("connection", (socket) => {
 });
 
 httpServer.listen(PORT, () => {
-  console.log(`Server is running on ${PORT} port`);
+  console.log(`Server #1 is running on ${PORT} port`);
 });
