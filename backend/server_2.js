8f05a3f8afef18cdd389b99373a0970ed62955c4
Mark Parker <marksadpepe@gmail.com>
2024-11-10 20:40:50 +0200

    initialized Firebase admin app to send FCM

--- backend/server_2.js
+++ backend/server_2.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 
 const dgram = require("dgram");
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
 
 const {SERVER_UDP_PORT, SERVER_UDP_HOST} = process.env;
 const port = parseInt(SERVER_UDP_PORT ?? "");
@@ -15,10 +17,25 @@ if (typeof port !== "number" || isNaN(port)) {
   process.exit(1)
 }
 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 const socket = dgram.createSocket("udp4");
 
-socket.on("message", (buf) => {
-  const {title, body} = JSON.parse(buf.toString());
+socket.on("message", async (buf) => {
+  const data = JSON.parse(buf.toString());
+  const message = {
+    token: data.token,
+    notification: {title: data.title, body: data.body},
+  }
+
+  try {
+    const response = await admin.messaging().send(message);
+    console.log(`FCM message sent: ${response}`);
+  } catch (err) {
+    console.error(`Failed to send FCM: ${err}`);
+  }
+
 });
 
 socket.bind(port, SERVER_UDP_HOST, () => {
